9b72b283117d0a76b0f3cb51ce93e10bacd0fa64
amywu23 <111457341+amywu23@users.noreply.github.com>
2023-01-04 20:09:05 +0000

    Co-authored-by: WadeNak <WadeNak@users.noreply.github.com>

--- tictactoe.java
+++ tictactoe.java
@@ -3,58 +3,97 @@ import javax.swing.*;
 import java.util.Random;
 import java.util.ArrayList;
 
-public class tictactoe extends JPanel {
 
+public class TicTacToe extends JPanel implements MouseListener,MouseMotionListener{
 
+   public static void main(String args[]) {
+    JFrame frame = new JFrame("TicTacToe");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+    TicTacToe panel = new TicTacToe();
+   
+    frame.add(panel);
+
+    frame.setSize(300, 200);
+    frame.setVisible(true);
+  }
+
+  public XandO(){
+      myFrame = new JFrame("XandO");
+        myFrame.add(this);
+        
+        constructImage(fileName);
+
+        // these eyes fit right on Einstein's head.  
+        playerX = new Eye(269, 290, 49, 37);
+        eye_2 = new Eye(372, 290, 49, 37);
+        addMouseListener(this);
+        addMouseMotionListener(this);
+        myFrame.setVisible(true);
+  }
+
+Random randGen = new Random();
 
 public void paintComponent(Graphics g) {
     super.repaint(); // don't worry about this -- it 'clears the screen'
 
-    g.setColor(Color.white);
-    g.fillRect(0, 0, windowWidth, windowHeight); // draw a nice white background
-    g.setColor(Color.black);
-    g.drawLine(startLine, 0, startLine, windowHeight); // draw a starting line
-    g.drawLine(startLine + trackLength, 0, startLine + trackLength, windowHeight); // finish
-    drawRacers(g);
+    g.drawLine(0,100,150,100);
+    g.drawLine(100,0,100,150);
+    g.drawLine(0, 50, 150, 50);
+    g.drawLine(50,0,50,150);
+    
+    
 }
 
+public void mouseClicked (MouseEvent event) {
+        eye_1.movePupil(event.getX(), event.getY());
+        eye_2.movePupil(event.getX(), event.getY());
+        
+        repaint();
+    }
 
-ArrayList<String> someNames = new ArrayList<String>() {
+    public void mouseMoved(MouseEvent event){
+        eye_1.movePupil(event.getX(), event.getY());
+        eye_2.movePupil(event.getX(), event.getY());
+        
+        repaint();
+    }
+    public void mouseDragged(MouseEvent event){
+        eye_1.movePupil(event.getX(), event.getY());
+        eye_2.movePupil(event.getX(), event.getY());
+        
+        repaint();
+    }
+  
+    public void mouseReleased (MouseEvent event) {
+    }
+    
+    public void mousePressed (MouseEvent event) {
+    }
+    
+    public void mouseEntered (MouseEvent event) {
+    }
+
+
+ArrayList<String> someSquares = new ArrayList<String>() {
     {
-       // this is a simple way of initializing an ArrayList in Java, but not
-       // something you need to know for this course.
-       // These are names of real race horses, as reported by 'the internet'.
-       add("dulcify");
-       add("whirlaway");
-       add("gumshoe");
-       add("skipteaser");
-       add("kissed by a fish");
-       add("rakatack");
-       add("viscosity");
-       add("flattermeforever");
-       add("sonneteer");
-       add("cigar starter");
-       add("flat out fast");
-       add("gothic soldier");
-       add("sexy librarian");
-       add("fishy advice");
-       add("motel princess");
-       add("lunatique");
-       add("panicking petunia");
-       add("wingspan");
-       add("southern missle");
-       add("excessive moves");
-       add("paul bunyans axe");
-       add("a storm it is");
-       add("dawn of war");
+       add("");
+       add("");
+       add("");
+       add("");
+       add("");
+       add("");
+       add("");
+       add("");
+       add("");
+
     }
  };
 
  // returns a random element from the someNames ArrayList
  public String randomName() {
     //FINISH THIS: MAKE SURE IT NEVER REPEATS until all used
-    return someNames.get(randGen.nextInt(someNames.size()));
+    return someSquares.get(randGen.nextInt(someSquares.size()));
  }
 
  ArrayList<Color> someColors = new ArrayList<Color>() {
@@ -68,7 +107,6 @@ ArrayList<String> someNames = new ArrayList<String>() {
        add(Color.yellow);
        add(Color.orange);
        add(Color.red);
-       add(Color.pink);
     }
  };
 
